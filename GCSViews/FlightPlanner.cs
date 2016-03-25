75185d55d2eedb801779048e1a5a33cc6bc2a278
Allen <bboypupu@gmail.com>
2016-03-25 19:02:24 +0800

    comment

--- GCSViews/FlightPlanner.cs
+++ GCSViews/FlightPlanner.cs
@@ -6299,7 +6299,10 @@ Column 1: Field type (RALLY is the only one at the moment -- may have RALLY_LAND
 
         }
 
-        
+        /*private class AutoLifeguards
+        {
+
+        }*/
 
         
 
